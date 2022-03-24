669d8b867755fefb38df1e108328e8f73a76f1ba
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-03-25 01:58:40 +0600

    cart section added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,37 @@
+/*  import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt='' />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header; */
+
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt=''></img>
+            <div>
+            <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
