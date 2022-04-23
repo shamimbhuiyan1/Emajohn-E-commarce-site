65cfa17ab8da8d928d0d110cc271520632d5ea97
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-04-23 22:08:19 +0600

    login section created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from "./Components/Header/Header";
 import Inventory from "./Components/Inventory/Inventory";
 import Orders from "./Components/Orders/Orders";
 import Shop from "./Components/Shop/Shop";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/SignUp/SignUp";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path="/orders" element={<Orders></Orders>}></Route>
         <Route path="/inventory" element={<Inventory></Inventory>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+const Login = () => {
+  return (
+    <div className="form-container">
+      <div>
+        <h2 className="form-title">Login</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="" />
+          </div>
+          <input className="form-submit" type="submit" value="Login" />
+        </form>
+        <p>
+          New to Ema-Jhon?{" "}
+          <Link className="form-link" to="/signup">
+            Create an account
+          </Link>
+        </p>
+        <div className="line">
+          <hr></hr>
+          <p className="or">or</p>
+          <hr></hr>
+        </div>
+        <button className="connect-google">Continue With Google</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
