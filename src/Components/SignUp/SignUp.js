9daf610e4a210dcb8777c5928223ab563add44e0
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-04-23 22:35:09 +0600

    signup created

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
   return (
-    <div>
-      <h2>SignUp</h2>
+    <div className="form-container">
+      <div>
+        <h2 className="form-title">Sign Up</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input type="password" name="password" id="" required />
+          </div>
+          <input className="form-submit" type="submit" value="Sign Up" />
+        </form>
+        <p>
+          Already have an account?{" "}
+          <Link className="form-link" to="/login">
+            Login
+          </Link>
+        </p>
+        <div className="line">
+          <hr></hr>
+          <p className="or">or</p>
+          <hr></hr>
+        </div>
+        <button className="connect-google">Continue With Google</button>
+      </div>
     </div>
   );
 };
