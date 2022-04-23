2b9061d52a4be5387638bc2f28820df7dca1eaf1
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-04-23 23:47:31 +0600

    handler section created

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,25 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 import "./SignUp.css";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const [createUserWithEmailAndPassword] =
+    useCreateUserWithEmailAndPassword(auth);
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordBlur = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordBlur = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleCreateUser = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Your two password did not match");
+      return;
+    }
+    if (password.length < 6) {
+      setError("Password must be 6 characters or longer");
+      return;
+    }
+    createUserWithEmailAndPassword(email, password);
+  };
   return (
     <div className="form-container">
       <div>
         <h2 className="form-title">Sign Up</h2>
-        <form>
+        <form onSubmit={handleCreateUser}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="" required />
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name="email"
+              id=""
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="" required />
+            <input
+              onBlur={handlePasswordBlur}
+              type="password"
+              name="password"
+              id=""
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="confirm-password">Confirm Password</label>
-            <input type="password" name="password" id="" required />
+            <input
+              onBlur={handleConfirmPasswordBlur}
+              type="password"
+              name="password"
+              id=""
+              required
+            />
           </div>
+          <p style={{ color: "red" }}>{error}</p>
           <input className="form-submit" type="submit" value="Sign Up" />
         </form>
         <p>
--- src/__auth-steps.js
+++ src/__auth-steps.js
@@ -1,8 +1,13 @@
 /**
- * 1.create new project in console.google.com
+ * 1.create new firebase project in console.google.com
  * 2.npm install firebase
- * 3.create firebase.init.js file created
+ * 3.create firebase.init.js file and import getAuth to export auth
  * 4.go to firebase settings> authentication >then enable email-password provider
+ * 5.Creating Login ,signUp component,setup route
+ * 6.attach form field handler and form submit handler
+ * 7.npm install --save react-firebase-hooks
+ * 8.useCreateUserWithEmailAndPassword(auth) fom react-firebase-hooks library
+
  */
 
 /**
